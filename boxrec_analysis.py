e990cefa99d19044a17cce8b2ed760a49fac14f3
Baudrillard <smbalf+222@gmail.com>
2022-10-20 16:16:17 +0100

    Update boxrec_analysis.py

--- boxrec_analysis.py
+++ boxrec_analysis.py
@@ -4,19 +4,16 @@ import os
 
 os.system('cls')
 
-boxrec_data = pd.read_csv('boxrec_tables.csv')
-
-df = pd.DataFrame(boxrec_data)
-
-div_list = ['heavy', 'cruiser', 'lightheavy', 'supermiddle', 'middle', 'superwelter', 'welter', 'superlight', 'light', 'feather', 'superbantam', 'bantam', 'superfly', 'fly', 'lightfly', 'minimum']
-
-df.insert(3, 'div index', '')
-
-x = 0
-
 def view_top_20(dataframe):
     print(dataframe[['name', 'division','bouts', 'rounds', 'KOs']].head(20))
 
+boxrec_data = pd.read_csv('boxrec_tables.csv')
+df = pd.DataFrame(boxrec_data)
+df.insert(3, 'div index', '')
+
+div_list = ['heavy', 'cruiser', 'lightheavy', 'supermiddle', 'middle', 'superwelter', 'welter', 'superlight', 'light', 'feather', 'superbantam', 'bantam', 'superfly', 'fly', 'lightfly', 'minimum']
+
+x = 0
 for value in df['division']:
     # print(f'{value} - {div_list.index(value)}')
     df.at[x, 'div index'] = div_list.index(value)
