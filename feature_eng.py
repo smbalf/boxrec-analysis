a711ee83b074df01be4497348a69be9f916b453d
Baudrillard <smbalf+222@gmail.com>
2022-12-14 16:24:46 +0000

    send it

--- feature_eng.py
+++ feature_eng.py
@@ -1,5 +1,12 @@
 import os
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 os.system('cls')
+
+dt = "13/05/2011"
+
+dto = datetime.strptime(dt, '%d/%m/%Y').date()
+
+print(dto)
